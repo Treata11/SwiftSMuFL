503ea65754cc997220137b0ae4186af6f92d3922
Treata11 <treata11@yahoo.com>
2023-12-03 15:35:06 +0330

    Add the decoded glyphnames from glyphnames.json.

--- Sources/SwiftSMuFL/MetadataTypes.swift
+++ Sources/SwiftSMuFL/MetadataTypes.swift
@@ -1,18 +1,33 @@
 /*
  See the LICENSE file for this sample’s licensing information.
- GlyphName.swift
+ MetadataTypes.swift
  SwiftSMuFL
 
  Created by Treata on 12/3/23.
  
  Abstract:
- 
+ Simple Types that act as the model of the JSON metadata
+ (ranges, glyphnames & classes).
 */
+
+// MARK: - class model
+
+struct Class: Codable {
+    
+}
+
+// MARK: - glyphname model
 
 struct GlyphName: Codable {
     let codepoint: String
     let alternateCodepoint: String?
     let description: String
+    
+    init(alternateCodepoint: String? = nil, codepoint: String, description: String) {
+        self.alternateCodepoint = alternateCodepoint
+        self.codepoint = codepoint
+        self.description = description
+    }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -20,4 +35,10 @@ struct GlyphName: Codable {
         self.alternateCodepoint = try container.decodeIfPresent(String.self, forKey: .alternateCodepoint)
         self.description = try container.decode(String.self, forKey: .description)
     }
+}
+
+// MARK: - range model
+
+struct Range: Codable {
+    
 }
